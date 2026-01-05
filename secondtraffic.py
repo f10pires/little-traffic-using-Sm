2bff28743238a4ae71cac6d5440a399f20d149ff
Felipe <f258434@dac.unicamp.br>
2026-01-05 20:32:53 -0300

    mudanças para inserção de onibus adicionada, cores aleatórias para carros

--- secondtraffic.py
+++ secondtraffic.py
@@ -15,10 +15,10 @@ def main():
     veh_counter = 0                                                                                 # vehicle counter
     random.seed(1)                                                                                  # reproduction
 
-    MAX_TIME = config["Max_time"]                                                                   # Extreme time of simulation (s)
+    MAX_TIME = config["Max_time"]                                                                   # extreme time of simulation (s)
 
     while traci.simulation.getTime() < MAX_TIME:
-        if int(traci.simulation.getTime()) % 2 == 0:                                                #return real time of simulation
+        if int(traci.simulation.getTime()) % 2 == 0:                                                # return real time of simulation
             addRandomVehicle(f"veh_{veh_counter}")
             veh_counter += 1
 
@@ -41,17 +41,19 @@ def startSim():
 
 """Create a vehicle with origin and destinetion """
 def addRandomVehicle(veh_id):
-    edges = possible_routes()
+    veh_type = random.choice(config["vehicles"])                                                   # aleatory vType
+    edges = possible_routes(veh_type)
 
     for travel in range(10):                                                                       # trying up to 10 times.
         from_edge = random.choice(edges)
         to_edge = random.choice(edges)
-        
+        color = (random.randint(50, 255),random.randint(50, 255),random.randint(50, 255))
+
         # Evitar origem = destino
         while to_edge == from_edge:
            to_edge = random.choice(edges)
 
-        route = traci.simulation.findRoute(from_edge, to_edge)
+        route = traci.simulation.findRoute(from_edge, to_edge, vType=veh_type)
 
         if not route.edges:
             continue                                                                              # try other couple
@@ -63,34 +65,48 @@ def addRandomVehicle(veh_id):
             traci.vehicle.add(
                 vehID=veh_id,
                 routeID=route_id,
+                typeID=veh_type,
                 depart=traci.simulation.getTime()
             )
-            return                                                                              # success
+            
+            if veh_type == "bus":
+                traci.vehicle.setColor(veh_id, (255, 0, 0))                                     # bus is red
+            else:
+                traci.vehicle.setColor(
+                    veh_id,
+                    (random.randint(50,255), random.randint(50,255), random.randint(50,255))
+                )
 
-    print(f"⚠️ It was not possible to create a route for {veh_id}")                             # In this case it's not possible to create a route
+            return                                                                               # success                                                                             
+
+    print(f"⚠️ It was not possible to create a route for {veh_id}")                             # in this case it's not possible to create a route
 
 """Surch the edges for routes"""
-def possible_routes():
+def possible_routes(veh_type):
     valid_edges = []
 
     for edge in traci.edge.getIDList():
 
-        
         if edge.startswith(":"):                                                                # ignore internals edges  (cintersections)
             continue
-
        
-        if traci.edge.getLaneNumber(edge) == 0:                                                  # It needs to have at least one lane.
+        if traci.edge.getLaneNumber(edge) == 0:                                                 # it needs to have at least one lane.
             continue
         
         for i in range(traci.edge.getLaneNumber(edge)):                                         # checks if any lane accepts cars or vehicles.
             lane_id = f"{edge}_{i}"
             allowed = traci.lane.getAllowed(lane_id)
+            
+            """Verify if the vehicle is permitted"""
+            if not allowed:
+                if veh_type not in config["RESTRICTED_TYPES"]:
+                    valid_edges.append(edge)
+                    break
 
-           
-            if not allowed or "passenger" in allowed:                                            # empty = all allowed
-                valid_edges.append(edge)
-                break
+            else:                                                                               # case 2: lane with restriction
+                if veh_type in allowed:
+                    valid_edges.append(edge)
+                    break
 
     return valid_edges
 
